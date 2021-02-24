3b039889bf4925df6417b8cfb3040bcfab2d5ffd
Miurell Cristina <miurellcristina@hotmail.es>
2021-02-23 19:20:57 -0800

    Variables y tipos de datos

--- Practica_2/Program.cs
+++ Practica_2/Program.cs
@@ -12,7 +12,27 @@ namespace Practica_2
         {
             //Mostrar Hello World en la consola
             System.Console.WriteLine("Hello World :D");
+
+            //Tomar los datos
             Console.ReadLine();
+
+            //Variables
+            var numero = 4;
+            var saludo = "Hola";
+            System.Console.WriteLine(numero);
+            System.Console.WriteLine(saludo);
+            System.Console.WriteLine(numero + 2);
+
+            //Tipos de variables
+            System.Int32 wholeNumber;
+            System.Double pi = 3.1416;
+            System.Boolean status = true;
+            System.String cadena = "hello";
+            System.Char letter = 'z';
+
+            //Espera a que se oprima una tecla para salir
+            Console.ReadKey();
+
         }
     }
 }
