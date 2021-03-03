0bc44adead718fec22bdefd81c702165fa873359
Miurell Cristina <miurellcristina@hotmail.es>
2021-03-02 19:37:03 -0800

    Ejercicio en clases para pedir nombre y edad (se investigo como guardar el valor solicitado)

---
+++ Ejercicio_StringInt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            int edad;
+            // Pedir nombre
+            Console.WriteLine("Por favor, ingresa tu nombre");
+            nombre = Console.ReadLine();
+
+            // Pedir Edad
+            Console.WriteLine("Por favor, ingresa tu edad");
+            edad = int.Parse(Console.ReadLine());
+
+            // Imprimir en consola
+            Console.WriteLine("Tu nombre es " + nombre + " y tienes " + edad + " años");
+            Console.WriteLine("Preciona cualquier tecla para salir");
+            Console.ReadKey();
+
+        }
+    }
+}
