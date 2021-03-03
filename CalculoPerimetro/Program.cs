1f81036b0b50c7c587a95005c5796110451c36a1
Miurell Cristina <miurellcristina@hotmail.es>
2021-03-02 19:00:35 -0800

    Se calculo el perimetro de un triangulo y se imprimio

---
+++ CalculoPerimetro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoPerimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Declaracion de variables 
+            int a = 10;
+            int b = 10;
+            int c = 7;
+            int p = a + b + c;
+
+            //2.Impresion en consola
+            Console.WriteLine("El perimetro del triangulo es: " + p);
+            Console.WriteLine("Presione cualquier tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
