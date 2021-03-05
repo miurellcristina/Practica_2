969a1302c70f31a894f6d59f6b5f3dba31a6db5a
Miurell Cristina <miurellcristina@hotmail.es>
2021-03-04 19:38:16 -0800

    Practicas de la clase del 4 de marzo

---
+++ Burguer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burguer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cantidad;
+            int producto1, producto2, producto3, resultado;
+            double total;
+            const double precioproducto1=45, precioproducto2=15, precioproducto3=20;
+
+            //Estructura de menu
+            Console.WriteLine("------------Menu-----------");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Codigo    Producto    Precio");
+            Console.WriteLine("  1     Hamburguesa     {0}", precioproducto1);
+            Console.WriteLine("  2        Bebida       {0}", precioproducto2);
+            Console.WriteLine("  1         Papas       {0}", precioproducto3);
+            Console.WriteLine("---------------------------");
+            
+            //Pedidos
+            Console.WriteLine("Cuantas hamburguesas desea?");
+            cantidad = Console.ReadLine();
+            producto1 = Convert.ToInt32(cantidad);
+
+            Console.WriteLine("Cuantas Bebidas desea?");
+            cantidad = Console.ReadLine();
+            producto2 = Convert.ToInt32(cantidad);
+
+            Console.WriteLine("Cuantas Papas desea?");
+            cantidad = Console.ReadLine();
+            producto3 = Convert.ToInt32(cantidad);
+
+
+            Console.WriteLine("------------Ticket------------");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Cantidad    Producto     Total");
+            resultado = producto1 * 45;
+
+            Console.WriteLine("  {0}       Hamburguesa     {1}", producto1, resultado);
+            resultado = producto2 * 15;
+
+            Console.WriteLine("  {0}          Bebida       {1}", producto2, resultado);
+
+            resultado = producto3 * 20;
+            Console.WriteLine("  {0}           Papas       {1}", producto3, resultado);
+            total = (producto1 * precioproducto1) + (producto2 * precioproducto2) + (producto3 * precioproducto3);
+
+            Console.WriteLine("              TOTAL       " + total);
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Preciona cualquier tecla para salir");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Operaciones/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string guardar_numero_1;
+            string guardar_numero_2;
+            int numero_2;
+            int numero_1;
+
+            Console.WriteLine("Escribe el primer numero");
+            guardar_numero_1 = Console.ReadLine();
+            numero_1 = Convert.ToInt32(guardar_numero_1);
+            Console.WriteLine("Escribe el segundo numero");
+            guardar_numero_2 = Console.ReadLine();
+            numero_2 = Convert.ToInt32(guardar_numero_2);
+
+            Console.WriteLine("Operaciones");
+
+            Console.WriteLine("La suma es: " + (numero_1 + numero_2));
+            Console.WriteLine("La resta es: " + (numero_1 - numero_2));
+            Console.WriteLine("La multiplicacion es: " + (numero_1 * numero_2));
+            Console.WriteLine("La division es: " + (numero_1 / numero_2));
+            Console.WriteLine("Preciona cualquier tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
